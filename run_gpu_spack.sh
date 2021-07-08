405a196b8e582535d51fdc581d5f1768a5256fa7
Olli Lupton <oliver.lupton@epfl.ch>
2021-07-08 15:07:46 +0200

    Improvements.

---
+++ run_gpu_spack.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+
+#SBATCH --account=proj16
+# SBATCH --partition=prod
+#SBATCH --time=01:00:00
+
+#SBATCH --nodes=1
+#SBATCH --constraint=volta
+#SBATCH --gres=gpu:4
+#SBATCH --ntasks-per-node=36
+
+#SBATCH --cpus-per-task=2
+#SBATCH --exclusive
+#SBATCH --mem=0
+
+# Stop on error
+#set -e
+
+# =============================================================================
+# SIMULATION PARAMETERS TO EDIT
+# =============================================================================
+
+# Using top level source and install directory, set the HOC_LIBRARY_PATH for simulator
+root_dir="${PWD}"
+output_prefix="${root_dir}/spack-outputs"
+BASE_DIR=$(pwd)/benchmark
+INSTALL_DIR=$BASE_DIR/install
+SOURCE_DIR=$BASE_DIR/sources
+
+export HOC_LIBRARY_PATH=$BASE_DIR/channels/lib/hoclib
+
+#Change this according to the desired runtime of the benchmark
+export SIM_TIME=5
+
+# Number of cells ((LCM of #cores_system1, #core_system2)*#cell_types)
+export NUM_CELLS=$((360*22))
+export NUM_CELLS=44
+
+# =============================================================================
+
+# Enter the channel benchmark directory
+cd $BASE_DIR/channels
+
+#rm -rf coredat_gpu
+#rm NRN_GPU.spk GPU_MOD2C.spk GPU_NMODL.spk
+#rm NRN_GPU.log GPU_MOD2C.log GPU_NMODL.log
+
+# Load external packages that only work because of $LD_LIBRARY_PATH
+module load unstable hpe-mpi
+# Load tools that we want to explicitly use (nsys)
+module load cuda
+module list
+
+# Load Spack after the above modules, so we don't pick up modules from our private Spack installation.
+. ~/bin/setup_spack_env.sh
+
+num_mpi=36
+neuron_spikes="${output_prefix}/neuron/NRN.spk"
+if [[ -f "${neuron_spikes}" ]]
+then
+  echo "Skipping creating ${neuron_spikes}"
+else
+  echo "----------------- NEURON SIM (CPU) ----------------"
+  (
+    spack env activate ${root_dir}/spack-envs/neuron
+    mkdir -p "${output_prefix}/neuron"
+    srun -n ${num_mpi} dplace ${root_dir}/spack-specials/neuron/x86_64/special -mpi -c arg_tstop=$SIM_TIME -c arg_target_count=$NUM_CELLS $HOC_LIBRARY_PATH/init.hoc |& tee "${output_prefix}/neuron/NRN.log"
+    sort -k 1n,1n -k 2n,2n > "${neuron_spikes}" < out.dat
+    rm out.dat
+  )
+fi
+
+coreneuron_input="${output_prefix}/coreneuron/coredat"
+if [[ -d "${coreneuron_input}" ]]
+then
+  echo "Skipping producing ${coreneuron_input}"
+else
+  echo "----------------- Produce coredat ----------------"
+  (
+    spack env activate ${root_dir}/spack-envs/neuron
+    mkdir -p "${output_prefix}/coreneuron"
+    srun -n ${num_mpi} dplace ${root_dir}/spack-specials/neuron/x86_64/special -mpi -c arg_dump_coreneuron_model=1 -c arg_tstop=$SIM_TIME -c arg_target_count=$NUM_CELLS $HOC_LIBRARY_PATH/init.hoc |& tee "${output_prefix}/coreneuron/NRN.log"
+    mv coredat/ "${coreneuron_input}"
+  )
+fi
+
+# =============================================================================
+nvidia-cuda-mps-control -d # Start the daemon
+
+echo "----------------- CoreNEURON SIM (GPU_MOD2C) ----------------"
+export CALI_CONFIG=nvtx,runtime-report
+(
+  spack env activate "${root_dir}/spack-envs/coreneuron_gpu_mod2c"
+  mkdir -p "${output_prefix}/coreneuron_gpu_mod2c"
+  command -v nsys
+  command -v mpirun
+  nsys profile --stats=true -o profile%p \
+    --verbose --wait=all --kill=none \
+    --capture-range=cudaProfilerApi \
+    --trace=cuda,nvtx,osrt,openacc,openmp \
+    srun -n ${num_mpi} dplace \
+    "${root_dir}/spack-specials/coreneuron_gpu_mod2c/x86_64/special-core" \
+    --voltage 1000. --mpi --gpu --cell-permute 2 --tstop $SIM_TIME -d "${coreneuron_input}" |& tee "${output_prefix}/coreneuron_gpu_mod2c/CNRN.log"
+  sort -k 1n,1n -k 2n,2n > "${output_prefix}/coreneuron_gpu_mod2c/CNRN.spk" < out.dat
+  rm out.dat
+)
+# Cleanup temporary files
+find /tmp/nvidia/nsight_systems -mindepth 1 -maxdepth 1 -user ${USER} -type d -print0 | xargs -0 rm -rf
+
+#echo "----------------- CoreNEURON SIM (GPU_NMODL) ----------------"
+#srun dplace $INSTALL_DIR/GPU_NMODL/special/x86_64/special-core --mpi --voltage 1000. --gpu --cell-permute 2 --tstop $SIM_TIME -d coredat_gpu 2>&1 | tee GPU_NMODL.log
+## Sort the spikes
+#cat out.dat | sort -k 1n,1n -k 2n,2n > GPU_NMODL.spk
+#rm out.dat
+
+echo quit | nvidia-cuda-mps-control
+# =============================================================================
+
+echo "---------------------------------------------"
+echo "-------------- Compare Spikes ---------------"
+echo "---------------------------------------------"
+
+DIFF=$(diff NRN_GPU.spk GPU_MOD2C.spk)
+if [ "$DIFF" != "" ] 
+then
+    echo "NRN_GPU.spk GPU_MOD2C.spk are not the same"
+else
+    echo "NRN_GPU.spk GPU_MOD2C.spk are the same"
+fi
+
+#DIFF=$(diff NRN_GPU.spk GPU_NMODL.spk)
+#if [ "$DIFF" != "" ] 
+#then
+#    echo "NRN_GPU.spk GPU_NMODL.spk are not the same"
+#else
+#    echo "NRN_GPU.spk GPU_NMODL.spk are the same"
+#fi
+
+# =============================================================================
+
+echo "---------------------------------------------"
+echo "----------------- SIM STATS -----------------"
+echo "---------------------------------------------"
+
+echo "Number of cells: $NUM_CELLS"
+echo "----------------- NEURON SIM STATS (CPU) ----------------"
+grep "psolve" NRN_GPU.log
+echo "----------------- CoreNEURON SIM (GPU_MOD2C) STATS ----------------"
+grep "Solver Time" GPU_MOD2C.log
+#echo "----------------- CoreNEURON SIM (GPU_NMODL) STATS ----------------"
+#grep "Solver Time" GPU_NMODL.log
+
+echo "---------------------------------------------"
+echo "---------------------------------------------"
--- spack_install.sh
+++ spack_install.sh
@@ -13,9 +13,11 @@ clone_project() {
 
 setup_environment() {
   env_name=$1
-  env_dir="${root_dir}/${env_name}"
-  mkdir "${env_dir}" || true
-  cp -v "${root_dir}/spack-template.yaml" "${env_dir}/spack.yaml"
+  env_dir="${root_dir}/spack-envs/${env_name}"
+  echo Removing ${env_dir}
+  rm -rf "${env_dir}"
+  mkdir -p "${env_dir}"
+  cp "${root_dir}/spack-template.yaml" "${env_dir}/spack.yaml"
   shift
   spack env activate "${env_dir}"
   for spec in "$@"
@@ -29,11 +31,14 @@ setup_environment() {
   done
   spack concretize -f
   spack install -j ${concurrent_jobs}
+  spack env deactivate
 }
+
 # Make sure Spack is available
 . ~/bin/setup_spack_env.sh
 
-# Make sure we have checkouts of the key projects. Spack falls over if you ask it to do this itself :-(
+# Make sure we have checkouts of the key projects.
+# Spack falls over if you ask it to do this itself :-(
 clone_project nmodl      git@github.com:BlueBrain/nmodl.git
 clone_project neuron     git@github.com:neuronsimulator/nrn.git
 clone_project coreneuron git@github.com:BlueBrain/CoreNeuron.git
@@ -44,35 +49,22 @@ CALIPER_SPEC="caliper%gcc@2.6.0:+cuda cuda_arch=70"
 CORENEURON_VARIANTS="+caliper~legacy-unit~report"
 setup_environment neuron neuron@develop%gcc~legacy-unit~rx3d
 setup_environment coreneuron_cpu_mod2c coreneuron@develop%intel${CORENEURON_VARIANTS}~gpu~ispc~nmodl~sympy "${CALIPER_SPEC}"
-setup_environment coreneuron_gpu_mod2c coreneuron@develop%nvhpc${CORENEURON_VARIANTS}+gpu~ispc~nmodl~sympy "${CALIPER_SPEC}"
+setup_environment coreneuron_gpu_mod2c coreneuron@develop%nvhpc@21.2${CORENEURON_VARIANTS}+gpu~ispc~nmodl~sympy "${CALIPER_SPEC}"
 setup_environment coreneuron_cpu_ispc  coreneuron@develop%intel${CORENEURON_VARIANTS}~gpu+ispc+nmodl+sympy "${CALIPER_SPEC}" "${NMODL_SPEC}"
 setup_environment coreneuron_cpu_nmodl coreneuron@develop%intel${CORENEURON_VARIANTS}~gpu~ispc+nmodl+sympy "${CALIPER_SPEC}" "${NMODL_SPEC}"
-setup_environment coreneuron_gpu_nmodl coreneuron@develop%nvhpc${CORENEURON_VARIANTS}+gpu~ispc+nmodl+sympy "${CALIPER_SPEC}" "${NMODL_SPEC}"
+setup_environment coreneuron_gpu_nmodl coreneuron@develop%nvhpc@21.2${CORENEURON_VARIANTS}+gpu~ispc+nmodl+sympy "${CALIPER_SPEC}" "${NMODL_SPEC}"
 
-#env_dir="channel-benchmark-test-env2"
-#spack_yaml="${env_dir}/spack.yaml"
-#mkdir "${env_dir}" || true
-#cp -v spack-template.yaml "${spack_yaml}"
-#create_environment_config_with_externals() {
-#  for spec in "$@"
-#  do
-#    if spack find "${spec}"
-#    then
-#      echo External ${spec} already found, not installing it.
-#    else
-#      echo Installing ${spec}
-#      spack install -j 8
-#for spec in "${CALIPER_SPEC}"
-#do
-#  if spack find "${spec}"
-#  then
-#    echo "${spec} already found"
-#  else
-#    echo "Installing ${spec}"
-#    spack install -j 8 "${spec}"
-#  fi
-#done
-#spack export --module tcl "${CALIPER_SPEC}" | sed -e 's/^/  /' >> "${spack_yaml}"
-#}
-#CALIPER_SPEC="caliper@2.6.0:+cuda cuda_arch=70"
+compile_mechs() {
+  env_name="$1"
+  nrnivmodl="$2"
+  working_dir="${root_dir}/spack-specials/${env_name}"
+  mkdir -p "${working_dir}"
+  (spack env activate "${root_dir}/spack-envs/${env_name}" && cd "${working_dir}" && "${nrnivmodl}" ${root_dir}/benchmark/channels/lib/modlib)
+}
 
+# Translate/compile the various mechanisms
+compile_mechs neuron nrnivmodl
+for coreneuron_env in coreneuron_cpu_mod2c coreneuron_gpu_mod2c coreneuron_cpu_ispc coreneuron_cpu_nmodl coreneuron_gpu_nmodl
+do
+  compile_mechs ${coreneuron_env} nrnivmodl-core
+done
