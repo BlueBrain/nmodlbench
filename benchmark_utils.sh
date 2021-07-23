5574d727cf09b65379697e87687500c05cb3cf61
Olli Lupton <oliver.lupton@epfl.ch>
2021-07-23 10:33:28 +0200

    Updates

--- benchmark_utils.sh
+++ benchmark_utils.sh
@@ -30,7 +30,7 @@ function run_benchmarks() {
 
   if [[ ${backend} == "gpu" ]]
   then
-    num_mpi=1 #4
+    num_mpi=1
   else
     num_mpi=${SLURM_TASKS_PER_NODE}
   fi
@@ -45,14 +45,14 @@ function run_benchmarks() {
   #Change this according to the desired runtime of the benchmark
   export SIM_TIME="0.725"
   export SIM_TIME="0.025"
-  export SIM_TIME=10
-  PRCELL_GID=1
+  export SIM_TIME=1000
+  PRCELL_GID=-1
 
   # Number of cells ((LCM of #cores_system1, #core_system2)*#cell_types)
   # OL: this was the original comment but we seem to be ignoring it. IIUC there are 22 cell types.
-  export NUM_CELLS=$((80*22))
-  export NUM_CELLS=16
-  #export NUM_CELLS=1
+  export NUM_CELLS=$((4*22))
+  #export NUM_CELLS=16
+  export NUM_CELLS=1
 
   # Load external packages.
   # OL: if we want to use an older CUDA it might need to be included here.
@@ -115,7 +115,7 @@ function run_benchmarks() {
       # This means we can run CPU builds with the GPU-targeted input files...  
       if [[ ${backend} == "gpu" && ${cnrn} == *"gpu"* ]]
       then
-        cell_permute=1
+        cell_permute=2
         gpu_arg="--gpu --cell-permute=${cell_permute}"
         prcellstate_slug="acc_gpu"
       else
@@ -127,11 +127,33 @@ function run_benchmarks() {
       for profile_mode in ${PROFILE_MODES}
       do
         echo "Running with profiler mode: ${profile_mode}"
+        mkdir -p "${root_dir}/output/profiles"
+        special_cmd="${root_dir}/spack-specials/${cnrn}/x86_64/special-core --voltage 1000. --mpi ${gpu_arg} --tstop ${SIM_TIME} -d ${cnrn_input_prefix}/coredat --prcellgid ${PRCELL_GID}"
+        profile_prefix="${output_profile_prefix}-${cnrn}-${profile_mode}"
+
+        echo Command: ${special_cmd}
 
         # nvtx: Caliper creates NVTX ranges that nsys can pick up
-        # runtime-report: Caliper prints a own profiling table at the end of execution
-        export CALI_CONFIG=nvtx,runtime-report
-  
+        # runtime-report: Caliper prints its own profiling table at the end of execution
+        #export CALI_CONFIG="runtime-report(calc.inclusive)"
+        #export CALI_SERVICES_ENABLE=debug
+        #export CALI_LOG_VERBOSITY=100
+
+        # Can also add this + mpiflush to CALI_SERVICES_ENABLE
+        # export CALI_MPIREPORT_WRITE_ON_FINALIZE=false
+
+        export CALI_CONFIG=nvtx
+        export CALI_CHANNEL_FLUSH_ON_EXIT=false
+        export CALI_MPIREPORT_FILENAME=${profile_prefix}-caliper.json
+        export CALI_MPIREPORT_LOCAL_CONFIG='select sum(sum#time.duration), inclusive_sum(sum#time.duration) group by prop:nested'
+        export CALI_MPIREPORT_CONFIG='SELECT annotation,
+                                             min(inclusive#sum#time.duration) AS "inclusive_time_rank_min",
+                                             max(inclusive#sum#time.duration) AS "inclusive_time_rank_max",
+                                             avg(inclusive#sum#time.duration) AS "inclusive_time_rank_avg",
+                                             inclusive_percent_total(sum#sum#time.duration) AS "Time %"
+                                             GROUP BY prop:nested FORMAT json'
+        export CALI_SERVICES_ENABLE=aggregate,event,mpi,mpireport,timestamp
+        
         # Tell nsys that --capture-range=nvtx can be triggered by an NVTX range whose
         # name is not a registered string. Caliper does not use registered strings.
         export NSYS_NVTX_PROFILER_REGISTER_ONLY=0
@@ -146,9 +168,6 @@ function run_benchmarks() {
           fi
         fi
   
-        mkdir -p "${root_dir}/output/profiles"
-        special_cmd="${root_dir}/spack-specials/${cnrn}/x86_64/special-core --voltage 1000. --mpi ${gpu_arg} --tstop ${SIM_TIME} -d ${cnrn_input_prefix}/coredat --prcellgid ${PRCELL_GID}"
-        profile_prefix="${output_profile_prefix}-${cnrn}-${profile_mode}"
         if [[ ${profile_mode} == "none" ]];
         then
           srun -n ${num_mpi} dplace ${special_cmd} |& tee "${profile_prefix}-0-CNRN.log"
@@ -168,13 +187,21 @@ function run_benchmarks() {
         fi
         mkdir -p "${output_prefix}/${cnrn}-${backend}"
         sort -k 1n,1n -k 2n,2n > "${output_prefix}/${cnrn}-${backend}/CNRN${suffix}.spk" < out.dat
-        formatted_sim_time=$(printf '%.6f' ${SIM_TIME})
-        for suffix in init t0.000000 t${formatted_sim_time}
-        do
-          filename="${PRCELL_GID}_${prcellstate_slug}_${suffix}.corenrn"
-          mv -v "${filename}" "${output_prefix}/${cnrn}-${backend}/${filename}"
-        done
         rm out.dat
+
+        # Add some metadata to the Caliper output JSON file
+        jq "{\"data\": ., \"build\": \"${cnrn}\", \"backend\": \"${backend}\"}" < "${CALI_MPIREPORT_FILENAME}" > "${CALI_MPIREPORT_FILENAME}.new"
+        mv "${CALI_MPIREPORT_FILENAME}.new" "${CALI_MPIREPORT_FILENAME}"
+
+        if [[ ${PRCELL_GID} > -1 ]]
+        then
+          formatted_sim_time=$(printf '%.6f' ${SIM_TIME})
+          for suffix in init t0.000000 t${formatted_sim_time}
+          do
+            filename="${PRCELL_GID}_${prcellstate_slug}_${suffix}.corenrn"
+            mv -v -- "${filename}" "${output_prefix}/${cnrn}-${backend}/${filename}"
+          done
+        fi
       done
     )
   done
--- spack_install.sh
+++ spack_install.sh
@@ -14,27 +14,40 @@ clone_project() {
 configured_environments=""
 setup_environment() {
   env_name=$1
-  env_dir="${root_dir}/spack-envs/${env_name}"
-  echo Removing ${env_dir}
-  rm -rf "${env_dir}"
-  mkdir -p "${env_dir}"
-  cp "${root_dir}/spack-template.yaml" "${env_dir}/spack.yaml"
-  shift
-  spack env activate "${env_dir}"
-  for spec in "$@"
-  do
-    package=$(echo ${spec} | cut -d @ -f 1)
-    spack add "${spec}"
-    if [[ -d "${root_dir}/spack-src-dirs/${package}" ]]
+  if [ -z "${SKIP_BUILD}" ]
+  then
+    env_dir="${root_dir}/spack-envs/${env_name}"
+    if [ -z "${SKIP_ENV}" ]
     then
-      spack develop -p "${root_dir}/spack-src-dirs/${package}" "${spec}"
+      echo Removing ${env_dir}
+      rm -rf "${env_dir}"
+      mkdir -p "${env_dir}"
+      cp "${root_dir}/spack-template.yaml" "${env_dir}/spack.yaml"
     fi
-  done
-  spack concretize -f
-  spack install -j ${concurrent_jobs}
-  spack env deactivate
+    spack env activate "${env_dir}"
+    if [ -z "${SKIP_ENV}" ]
+    then
+      spack add jq%gcc
+      shift
+      for spec in "$@"
+      do
+        package=$(echo ${spec} | cut -d @ -f 1)
+        spack add "${spec}"
+        if [[ -d "${root_dir}/spack-src-dirs/${package}" ]]
+        then
+          spack develop -p "${root_dir}/spack-src-dirs/${package}" "${spec}"
+        fi
+      done
+      spack concretize -f
+    fi
+    spack install -j ${concurrent_jobs}
+    spack env deactivate
+  fi
   configured_environments="${configured_environments} ${env_name}"
 }
+
+# Make sure a modern GCC is first in $PATH
+module load unstable gcc
 
 # Make sure Spack is available
 . ~/bin/setup_spack_env.sh
@@ -47,12 +60,12 @@ clone_project coreneuron git@github.com:BlueBrain/CoreNeuron.git
 
 # Create and install the various environments.
 NMODL_SPEC="nmodl@develop%gcc~legacy-unit"
-CUDA_SPEC="cuda@11.3.1%gcc ^libiconv%gcc"
+CUDA_SPEC="cuda@11.4.0%gcc ^libiconv%gcc"
 CALIPER_SPEC="caliper%gcc@2.6.0:+cuda cuda_arch=70"
 CORENEURON_VARIANTS="+caliper~legacy-unit~report"
 setup_environment neuron neuron@develop%gcc~legacy-unit~rx3d
 setup_environment coreneuron_cpu_mod2c coreneuron@develop%intel${CORENEURON_VARIANTS}~gpu~ispc~nmodl~sympy "${CALIPER_SPEC}"
-setup_environment coreneuron_gpu_mod2c coreneuron@develop%nvhpc@21.5${CORENEURON_VARIANTS}+gpu~ispc~nmodl~sympy "${CALIPER_SPEC}" "${CUDA_SPEC}"
+setup_environment coreneuron_gpu_mod2c coreneuron@develop%nvhpc@21.7${CORENEURON_VARIANTS}+gpu~ispc~nmodl~sympy "${CALIPER_SPEC}" "${CUDA_SPEC}"
 #setup_environment coreneuron_cpu_mod2c_nvhpc coreneuron@develop%nvhpc@21.5${CORENEURON_VARIANTS}~gpu~ispc~nmodl~sympy "${CALIPER_SPEC}"
 #setup_environment coreneuron_cpu_mod2c_nvhpc_debug "coreneuron@develop%nvhpc@21.5${CORENEURON_VARIANTS}~gpu~ispc~nmodl~sympy build_type=Debug" "${CALIPER_SPEC}"
 #setup_environment coreneuron_gpu_mod2c_debug "coreneuron@develop%nvhpc@21.5${CORENEURON_VARIANTS}+gpu~ispc~nmodl~sympy build_type=Debug" "${CALIPER_SPEC}" "${CUDA_SPEC}"
